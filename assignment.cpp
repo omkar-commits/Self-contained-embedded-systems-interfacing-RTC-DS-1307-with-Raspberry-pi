060c227861f1b369cd483f00c3bff337a0d15565
Omkar Pabshetwar <omkar.pabshetwar2@mail.dcu.ie>
2019-03-10 21:58:14 +0000

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -5,7 +5,135 @@
 // Copyright   : Your copyright notice
 // Description : AssignmentEE513, Ansi-style
 //============================================================================
+#include <iostream>
+#include<cstdio>
+#include<linux/i2c-dev.h>
+#include<linux/i2c.h>
+#include<string.h>
+#include<fcntl.h>  // THis is used for O_RDWR
+#include<unistd.h>  // This used for open()
+#include<sys/ioctl.h>
+using namespace std;
+#define BUFFER_SIZE 19      //0x00 to 0x12
+#define HEX(x) setw(2) << setfill('0') << hex << (int)(x)
+int bcdToDec(char b) { return (b/16)*10 + (b%16); }
 
+class Rpi2c{
+protected:
+	int i2cfile;
+public:
+	Rpi2c(int);
+	//int file;
+	int address= 0x68;
+	int I2C_SLAVE;
+	int rtc_w();
+	int rtc_r();
+	//int rtcdata;
+	int number=7;
+	int data=0;
+	unsigned char value;
+	unsigned char buffer[BUFFER_SIZE];
+	//virtual void close();
+	virtual ~Rpi2c();
+};
+
+ Rpi2c::Rpi2c(int address)
+ {
+		char filename[11] = "/dev/i2c-1";
+
+	 i2cfile= open(filename, O_RDWR);
+
+	 if(i2cfile <0){
+		 cout << "Failed to open the bus" << endl;
+	 }
+
+	 if(ioctl(i2cfile, I2C_SLAVE, address) <0){
+		 perror("failed to connect to the sensor\n");
+
+		 cout << "For slave I2C address can't be set" << endl;
+
+	 }
+ }
+
+ class rtc_w: public Rpi2c{
+
+ private:
+	 int rtcdata;
+public:
+
+	 int data;
+	// unsigned char value;
+
+ };
+ int Rpi2c::rtc_w(int rtcdata){
+	 unsigned char buffer[1];
+	 buffer[0]=value;
+	rtcdata= write(i2cfile, buffer, 1);
+
+	if(rtcdata !=1){
+		perror("I2c failed to write the device");
+		return 1;
+	}
+	return 0;
+ }
+
+ class rtc_r: public Rpi2c{
+
+ private:
+	 int rtcdata;
+public:
+
+	 int data;
+	// unsigned char value;
+
+ };
+ int Rpi2c::rtc_r(int rtcdata){
+ 	// unsigned char* data= new unsigned char [number];
+ 	 buffer[0]=value;
+ 	rtcdata= read(i2cfile, buffer, (int) number);
+
+ 	if(rtcdata !=(int)number){
+ 		perror("I2c failed to read data from the device");
+ 		return NULL;
+ 	}
+ 	else           // return data
+ 			{
+
+ 				char seconds = buffer[0];
+ 				char minutes = buffer[1];
+ 				char hours = buffer[2];
+ 				char dayOfWeek = buffer[3];
+ 				char day = buffer[4];
+ 				char month = buffer[5];
+ 				char year = buffer[6];
+
+ 				cout << "The Time in RTC is:" << endl;
+ 				cout << "Date Y/M/D:"<<  bcdToDec(year)<<"-"<<  bcdToDec(month)<< "-"<<  bcdToDec(day)<<endl;
+ 				cout << "Time H/M/S: "<<  bcdToDec(hours)<< "-"<<   bcdToDec(minutes)<< "-"<<  bcdToDec(seconds)<<endl;
+  }
+
+ return 0;
+ }
+
+ void Rpi2c::close(){
+	 close (i2cfile);
+	 i2cfile=-1;
+ }
+ Rpi2c::~Rpi2c(){
+	 if(i2cfile != -1)
+		 close (i2cfile);
+
+ }
+
+ int main (){
+
+	 Rpi2c date;
+	 date.rtc_w;
+	 date.rtc_r;
+	 return 0;
+ }
+
+/*
 #include <iostream>
 #include<linux/i2c-dev.h>
 #include<linux/i2c.h>
@@ -84,7 +212,7 @@ int main (){
 
 }
 
-/*#include<stdio.h>
+#include<stdio.h>
 #include<fcntl.h>
 #include<sys/ioctl.h>
 #include<linux/i2c.h>
