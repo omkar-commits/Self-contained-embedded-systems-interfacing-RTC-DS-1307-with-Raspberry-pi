4e1bcda3683a80bae0ad18ae2a6025164a127ce5
Omkar Pabshetwar <omkar.pabshetwar2@mail.dcu.ie>
2019-03-12 13:01:09 +0000

    Update assignment.cpp

--- assignment.cpp
+++ assignment.cpp
@@ -11,6 +11,7 @@ using namespace std;
 #define BUFFER_SIZE 19      //0x00 to 0x12
 #define HEX(x) setw(2) << setfill('0') << hex << (int)(x)
 int bcdToDec(char b) { return (b/16)*10 + (b%16); }
+int decToBcd (char b) { return (b/10*16) + (b%10); }
 int i2cfile;
 class Rpi2c{
 protected:
@@ -50,9 +51,21 @@ public:
  int Rpi2c::rtc_w(){
 	// unsigned char buffer[1];
 	 buffer[0]=value;
-	rtcdata= write(i2cfile, buffer, 1);
+	 buffer[1]=0x01;
+	 buffer[2]=0x02;
+	 buffer[3]=0x03;
+	 buffer[4]=0x04;
+	 buffer[5]=0x05;
+	 buffer[6]=0x06;
+	 buffer[7]=0x07;
+	/* buffer[8]=0x08;
+	 buffer[9]=0x09;
+	 buffer[10]=0x10;
+	 buffer[11]=0x11;
+	 buffer[12]=0x12;*/
+	rtcdata= write(i2cfile, buffer, 7);
 
-	if(rtcdata !=1){
+	if(rtcdata !=7){
 		perror("I2c failed to write the device");
 return 1;
 	}
